413abd0b164af4c6b5862e5771eac3a1c18c6f80
badboy1496 <36558440+badboy1496@users.noreply.github.com>
2018-03-21 23:22:23 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,9 @@
+package game;
+
+public class Client {
+
+	public static void main(String[] args) {
+		TypingTutor obj = new TypingTutor("Oregon Caves National Monument and Preserve is a protected area in the northern Siskiyou Mountains of southwestern Oregon near the California border, managed since 1933 by the US National Park Service. The 4,554-acre (1,843 ha) park features a marble cave that was discovered in 1874. Three years after President Theodore Roosevelt signed the Antiquities Act of 1906, President William Howard Taft used it to establish Oregon Caves. In 2014 the protected area was expanded by about 4,000 acres (1,600 ha) and designated a National Monument and Preserve. Oregon Caves is a solutional cave, with passages totaling about 15,000 feet (4,600 m), formed in marble. The parent rock was originally limestone that metamorphosed to marble during the geologic processes that created the Klamath Mountains, including the Siskiyous. Although the limestone formed about 190 million years ago, the cave itself is no older than a few million years");
+	}
+
+}
